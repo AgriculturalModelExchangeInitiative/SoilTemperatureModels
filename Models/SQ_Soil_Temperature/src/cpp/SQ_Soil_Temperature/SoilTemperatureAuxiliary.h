e8dfe55b8e4b0f7c5fc1e702b7768e57063d701b
cyrillemidingoyi <ahmedmidingoyi@yahoo.fr>
2024-04-24 10:47:56 +0200

    update

---
+++ Models/SQ_Soil_Temperature/src/cpp/SQ_Soil_Temperature/SoilTemperatureAuxiliary.h
@@ -0,0 +1,15 @@
+#pragma once
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <string>
+namespace SQ_Soil_Temperature {
+class SoilTemperatureAuxiliary
+{
+    private:
+    public:
+        SoilTemperatureAuxiliary();
+
+};
+}
