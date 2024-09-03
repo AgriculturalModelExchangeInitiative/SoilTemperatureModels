b14b273332cf5299a910f836d65fc29306d112e2
Teiki RAIHAUTI <raihauti@supagro.inra.lan>
2024-09-03 14:19:53 +0200

    add inputs for campbell soil model

--- Models/campbell_SoilTemp/SoilTemperature.py
+++ Models/campbell_SoilTemp/SoilTemperature.py
@@ -26,6 +26,181 @@ def model_SoilTemperatureCampbell():
                 * ShortDescription: TODO
     
     - inputs: 
+         * name: NLAYR
+               ** description : number of soil layers in profile
+               ** inputtype : parameter
+               ** variablecategory : constant
+               ** datatype : INT
+               ** max :
+               ** min : 1
+               ** default : 10
+               ** unit : dimensionless
+         * name: THICK
+               ** description : APSIM soil layer depths as thickness of layers
+               ** inputtype : parameter
+               ** variablecategory : constant
+               ** datatype : DOUBLEARRAY
+               ** len : NLAYR
+               ** max :
+               ** min : 1
+               ** default : 5
+                       ** unit : mm 
+        * name: BD
+               ** description : bd (soil bulk density) is name of the APSIM var for bulk density so set bulkDensity = bd later
+               ** inputtype : parameter
+               ** variablecategory : constant
+               ** datatype : DOUBLEARRAY
+               ** len : NLAYR
+               ** max :
+               ** min : 1
+               ** default : 
+               ** unit : g/cm3
+        * name: TMAX
+               ** description : Max daily Air temperature
+               ** inputtype : variable
+               ** variablecategory : exogenous
+               ** datatype : DOUBLE
+               ** max : 60
+               ** min : -60
+               ** default :
+               ** unit : °C
+        * name: TMIN
+               ** description : Min daily Air temperature
+               ** inputtype : variable
+               ** variablecategory : exogenous
+               ** datatype : DOUBLE
+               ** max : 60
+               ** min : -60
+               ** default :
+               ** unit : °C
+        * name: TAV
+               ** description : Average daily Air temperature
+               ** inputtype : variable
+               ** variablecategory : exogenous
+               ** datatype : DOUBLE
+               ** max : 60
+               ** min : -60
+               ** default :
+               ** unit : °C
+        * name: TAMP
+               ** description : Amplitude air temperature
+               ** inputtype : parameter
+               ** variablecategory : constant
+               ** datatype : DOUBLE
+               ** max : 100
+               ** min : -100
+               ** default :
+               ** unit : °C
+        * name: XLAT
+               ** description : Latitude
+               ** inputtype : parameter
+               ** variablecategory : constant
+               ** datatype : DOUBLE
+               ** max : 
+               ** min :
+               ** default :
+               ** unit : °C
+        * name: CLAY
+               ** description : Proportion of clay in each layer of profile
+               ** inputtype : parameter
+               ** variablecategory : constant
+               ** datatype : DOUBLEARRAY
+               ** len : NLAYR
+               ** max : 1
+               ** min : 0
+               ** default : 0.5
+               ** unit : dimensionless
+        * name: SW
+               ** description : volumetric water content
+               ** inputtype : variable
+               ** variablecategory : exogenous
+               ** datatype : DOUBLEARRAY
+               ** len : NLAYR
+               ** max : 1
+               ** min : 0
+               ** default : 0.5
+               ** unit : cc water / cc soil
+        * name: DEPTH
+               ** description : node depths
+               ** inputtype : parameter
+               ** variablecategory : constant
+               ** datatype : DOUBLEARRAY
+               ** len : NLAYR
+               ** max :
+               ** min : 
+               ** default :
+               ** unit : m
+        * name: DOY
+               ** description : Day of year
+               ** inputtype : variable
+               ** variablecategory : exogenous
+               ** datatype : DOUBLE
+               ** max : 366
+               ** min : 0
+               ** default : 0
+               ** unit : dimensionless
+        * name: canopyHeight
+               ** description : height of canopy above ground
+               ** inputtype : variable
+               ** variablecategory : exogenous
+               ** datatype : DOUBLE
+               ** max : 
+               ** min : 0
+               ** default : 0.057
+               ** unit : m
+        * name: SALB
+               ** description : Soil albedo
+               ** inputtype : parameter
+               ** variablecategory : constant
+               ** datatype : DOUBLE
+               ** max : 1
+               ** min : 0
+               ** default : 
+               ** unit : dimensionless
+        * name: SRAD
+               ** description : Radiation
+               ** inputtype : variable
+               ** variablecategory : exogenous
+               ** datatype : DOUBLE
+               ** max : 
+               ** min : 
+               ** default : 
+               ** unit : 
+        * name: ESP
+               ** description : Potential evaporation
+               ** inputtype : variable
+               ** variablecategory : exogenous
+               ** datatype : DOUBLE
+               ** max : 
+               ** min : 0
+               ** default : 
+               ** unit : mm
+        * name: EOAD
+               ** description : Potential evapotranspiration
+               ** inputtype : variable
+               ** variablecategory : exogenous
+               ** datatype : DOUBLE
+               ** max : 
+               ** min : 0
+               ** default : 
+               ** unit : mm
+        * name: ESAD
+               ** description : Actual evapotranspiration
+               ** inputtype : variable
+               ** variablecategory : exogenous
+               ** datatype : DOUBLE
+               ** max : 
+               ** min : 0
+               ** default : 
+               ** unit : mm
+         * name: soilTemp
+                ** description :  Temperature at end of last time-step within a day - midnight in layers
+                ** variablecategory : state
+                ** datatype : DOUBLEARRAY
+                ** min : -60.
+                ** max : 60.
+                ** unit : degC
+                ** uri : 
 
     - outputs: 
                 * name: soilTemp
