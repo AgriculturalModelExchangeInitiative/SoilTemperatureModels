e5372f7d183e8d183de090d0b987db9e28902869
Teiki RAIHAUTI <raihauti@supagro.inra.lan>
2024-09-04 16:31:14 +0200

    init function in progress

--- Models/campbell_SoilTemp/InitCampbellInProgress.py
+++ Models/campbell_SoilTemp/InitCampbellInProgress.py
@@ -1,19 +1,48 @@
 import numpy as np
+from math import isnan,isinf,pi,sqrt,sin,exp
 
-def init_campbell(nbLayers, thick, bd, tmax, tmin, cla, sw, dpth, tav, tamp, doy, canopHeight, SALB, rad, XLAT, AltitudeMetres):
+def init_campbell((NLAYR: int,
+    THICK: 'Array[float]',
+    BD: 'Array[float]',
+    TMAX: float,
+    TMIN: float,
+    TAV: float,
+    TAMP: float,
+    XLAT: float,
+    CLAY: 'Array[float]',
+    SW: 'Array[float]',
+    DEPTH: 'Array[float]',
+    DOY: float,
+    canopyHeight: float,
+    SALB: float,
+    SRAD: float,
+    ESP: float,
+    EOAD: float,
+    ESAD: float,
+    soilTemp: 'Array[float]'):
+ 
+    #constants
+    soilRoughnessHeight = 57.0
+    AltitudeMetres = 18.0
+    NUM_PHANTOM_NODES = 5
+    CONSTANT_TEMPdepth = 10.0
+    AIRnode = 0
+    SURFACEnode = 1
+    TOPSOILnode = 2
+
     SALB = SALB
     canopyHeight = max(canopyHeight, soilRoughnessHeight) * 0.001
     airPressure = 101325.0 * (1.0 - 2.25577e-5 * AltitudeMetres) ** 5.25588 * 0.01
-    numLayers = nbLayers
-    numNodes = nbLayers + NUM_PHANTOM_NODES
+    NLAYR = NLAYR
+    numNodes = NLAYR + NUM_PHANTOM_NODES
 
-    thickness = array('f', [0.0]*(numLayers + 1 + NUM_PHANTOM_NODES))
-    thickness[:len(thick)] = thick
-    sumThickness = sum(thickness[1:numLayers + 1])
+    thickness = array('f', [0.0]*(NLAYR + 1 + NUM_PHANTOM_NODES))
+    thickness[:len(THICK)] = THICK
+    sumThickness = sum(thickness[1:NLAYR + 1])
     BelowProfileDepth = max(CONSTANT_TEMPdepth * 1000.0 - sumThickness, 1.0 * 1000.0)
 
     thicknessForPhantomNodes = BelowProfileDepth * 2.0 / NUM_PHANTOM_NODES
-    firstPhantomNode = numLayers
+    firstPhantomNode = NLAYR
     for i in range(firstPhantomNode, firstPhantomNode + NUM_PHANTOM_NODES):
         thickness[i] = thicknessForPhantomNodes
 
@@ -28,28 +57,28 @@ def init_campbell(nbLayers, thick, bd, tmax, tmin, cla, sw, dpth, tav, tamp, doy
         depth[node + 1] = (sumThickness + 0.5 * thickness[node]) * 0.001
 
     # Bulk Density
-    bulkDensity = array('f', [0.0]*(numLayers + 1 + NUM_PHANTOM_NODES)
-    bulkDensity[:min(numLayers + 1 + NUM_PHANTOM_NODES, len(bd))] = bd
-    bulkDensity[numNodes - 1] = bulkDensity[numLayers]
-    for layer in range(numLayers + 1, numLayers + NUM_PHANTOM_NODES):
-        bulkDensity[layer] = bulkDensity[numLayers]
+    bulkDensity = array('f', [0.0]*(NLAYR + 1 + NUM_PHANTOM_NODES)
+    bulkDensity[:min(NLAYR + 1 + NUM_PHANTOM_NODES, len(bd))] = bd
+    bulkDensity[numNodes - 1] = bulkDensity[NLAYR]
+    for layer in range(NLAYR + 1, NLAYR + NUM_PHANTOM_NODES):
+        bulkDensity[layer] = bulkDensity[NLAYR]
 
     # Soil Water
-    soilWater = array('f', [0.0]*(numLayers + 1 + NUM_PHANTOM_NODES)
-    soilWater[:min(numLayers + 1 + NUM_PHANTOM_NODES, len(sw))] = sw
-    for layer in range(numLayers + 1, numLayers + NUM_PHANTOM_NODES):
-        soilWater[layer] = soilWater[numLayers]
+    soilWater = array('f', [0.0]*(NLAYR + 1 + NUM_PHANTOM_NODES)
+    soilWater[:min(NLAYR + 1 + NUM_PHANTOM_NODES, len(sw))] = sw
+    for layer in range(NLAYR + 1, NLAYR + NUM_PHANTOM_NODES):
+        soilWater[layer] = soilWater[NLAYR]
 
     # Clay
-    clay = array('f', [0.0]*(numLayers + 1 + NUM_PHANTOM_NODES)
-    for layer in range(1, numLayers + 1):
+    clay = array('f', [0.0]*(NLAYR + 1 + NUM_PHANTOM_NODES)
+    for layer in range(1, NLAYR + 1):
         clay[layer] = cla[layer - 1]
-    for layer in range(numLayers + 1, numLayers + NUM_PHANTOM_NODES):
-        clay[layer] = clay[numLayers]
+    for layer in range(NLAYR + 1, NLAYR + NUM_PHANTOM_NODES):
+        clay[layer] = clay[NLAYR]
 
-    maxSoilTemp = array('f', [0.0]*(numLayers + 1 + NUM_PHANTOM_NODES))
-    minSoilTemp = array('f', [0.0]*(numLayers + 1 + NUM_PHANTOM_NODES))
-    aveSoilTemp = array('f', [0.0]*(numLayers + 1 + NUM_PHANTOM_NODES))
+    maxSoilTemp = array('f', [0.0]*(NLAYR + 1 + NUM_PHANTOM_NODES))
+    minSoilTemp = array('f', [0.0]*(NLAYR + 1 + NUM_PHANTOM_NODES))
+    aveSoilTemp = array('f', [0.0]*(NLAYR + 1 + NUM_PHANTOM_NODES))
     volSpecHeatSoil = array('f', [0.0]*(numNodes + 1))
     soilTemp = array('f', [0.0]*(numNodes + 1 + 1))
     morningSoilTemp = array('f', [0.0]*(numNodes + 1 + 1))
@@ -58,44 +87,78 @@ def init_campbell(nbLayers, thick, bd, tmax, tmin, cla, sw, dpth, tav, tamp, doy
     heatStorage = array('f', [0.0]*(numNodes + 1))
     thermalConductance = array('f', [0.0]*(numNodes + 1 + 1))
 
-    doThermalConductivityCoeffs(nbLayers)
-    latitude = XLAT
-    CalcSoilTemp(tav, tamp, doy, latitude)
-    np.copyto(tempNew, soilTemp)
+    thermCondPar1,thermCondPar2,thermCondPar3,thermCondPar4 = doThermalConductivityCoeffs(NLAYR, numNodes, bulkDensity, clay)
+    soilTemp = CalcSoilTemp(soilTemp, thickness, TAV, TAMP, DOY, XLAT)
+    #tempNew = list(soilTemp)
 
-    InitialValues = array('f', [0.0]*(numLayers))
-    np.copyto(InitialValues, soilTemp[TOPSOILnode:TOPSOILnode + numLayers])
+    InitialValues = array('f', [0.0]*(NLAYR))
+    InitialValues = list(soilTemp[TOPSOILnode:TOPSOILnode + NLAYR])
 
-    ave_temp = (tmax + tmin) * 0.5
-    surfaceT = (1.0 - albedo) * (ave_temp + (tmax - ave_temp) * np.sqrt(max(rad, 0.1) * 23.8846 / 800.0)) + albedo * ave_temp
+    ave_temp = (TMAX + TMIN) * 0.5
+    surfaceT = (1.0 - SALB) * (ave_temp + (TMAX - ave_temp) * math.sqrt(max(SRAD, 0.1) * 23.8846 / 800.0)) + SALB * ave_temp
     soilTemp[SURFACEnode] = surfaceT
 
     for i in range(numNodes + 1, len(soilTemp)):
-        soilTemp[i] = tav
+        soilTemp[i] = TAV
 
-    np.copyto(tempNew, soilTemp)
+    #np.copyto(tempNew, soilTemp)
 
-    minTempYesterday = tmin
-    maxTempYesterday = tmax
+    minTempYesterday = TMIN
+    maxTempYesterday = TMAX
 
 
 
-def doThermalConductivityCoeffs(nbLayers):
+def doThermalConductivityCoeffs(nbLayers:int, 
+                                numNodes:int,
+                                bulkDensity: 'Array[float]',
+                                 clay: 'Array[float]'):
 
     thermCondPar1 = array('f', [0.0]*(numNodes + 1))
     thermCondPar2 = array('f', [0.0]*(numNodes + 1))
     thermCondPar3 = array('f', [0.0]*(numNodes + 1))
     thermCondPar4 = array('f', [0.0]*(numNodes + 1))
 
-    for layer in range(1, int(nbLayers) + 2):
+    for layer in range(1, nbLayers + 2):
         element = layer
         thermCondPar1[element] = 0.65 - 0.78 * bulkDensity[layer] + 0.6 * bulkDensity[layer] ** 2
         thermCondPar2[element] = 1.06 * bulkDensity[layer]
-        thermCondPar3[element] = 1.0 + Divide(2.6, np.sqrt(clay[layer], 0)
+        thermCondPar3[element] = 1.0 + Divide(2.6, math.sqrt(clay[layer], 0)
         thermCondPar4[element] = 0.03 + 0.1 * bulkDensity[layer] ** 2
+
+        return thermCondPar1,thermCondPar2,thermCondPar3,thermCondPar4
+
+
+def CalcSoilTemp(soilTempIO: 'Array[float]',
+                 thickness: 'Array[float]',
+                tav:float,
+                tamp:float, 
+                doy:int, 
+                latitude:float):
+
+    cumulativeDepth = array('f', [0.0]*(len(thickness)))
+    if len(thickness) > 0:
+        cumulativeDepth[0] = thickness[0]
+        for Layer in range(1, len(thickness)):
+            cumulativeDepth[Layer] = thickness[Layer] + cumulativeDepth[Layer - 1]
+
+    w = 2 * math.pi / (365.25 * 24 * 3600)
+    dh = 0.6
+    zd = math.sqrt(2 * dh / w)
+    offset = 0.25
+    if latitude > 0.0:
+        offset = -0.25
+
+    soilTemp = array('f', [0.0]*(numNodes + 2))
+    for nodes in range(1, numNodes + 1):
+        soilTemp[nodes] = tav + tamp * math.exp(-1 * cumulativeDepth[nodes] / zd) * math.sin((doy / 365.0 + offset) * 
+                                                                                             2.0 * math.pi - cumulativeDepth[nodes] / zd)
+
+    soilTempIO[SURFACEnode:SURFACEnode + numNodes] = soilTemp[0:numNodes]
+    return soilTempIO
+
 
 def Divide(val1, val2, errVal):
     returnValue = val1 / val2
-    if math.isinf(returnValue) or math.isnan(returnValue):    
+    if isinf(returnValue) or isnan(returnValue):    
         return errVal
     return returnValue
