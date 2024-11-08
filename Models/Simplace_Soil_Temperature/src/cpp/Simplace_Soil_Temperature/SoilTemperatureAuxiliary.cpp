3abb0c260b3b6e2eb9a0e3588aa1c4d9bc906fa1
Michael Berg-Mohnicke <michael.berg@zalf.de>
2024-11-08 13:17:41 +0100

    soil temperature model results are ok now

--- Models/Simplace_Soil_Temperature/src/cpp/Simplace_Soil_Temperature/SoilTemperatureAuxiliary.cpp
+++ Models/Simplace_Soil_Temperature/src/cpp/Simplace_Soil_Temperature/SoilTemperatureAuxiliary.cpp
@@ -3,6 +3,16 @@ using namespace Simplace_Soil_Temperature;
 
 SoilTemperatureAuxiliary::SoilTemperatureAuxiliary() {}
 
+std::vector<double> & SoilTemperatureAuxiliary::getSoilTempArray() { return this->SoilTempArray; }
+std::vector<double> & SoilTemperatureAuxiliary::getiSoilTempArray() { return this->iSoilTempArray; }
 double SoilTemperatureAuxiliary::getSnowIsolationIndex() { return this->SnowIsolationIndex; }
+double SoilTemperatureAuxiliary::getiSoilSurfaceTemperature() { return this->iSoilSurfaceTemperature; }
 
+void SoilTemperatureAuxiliary::setSoilTempArray(std::vector<double> const &_SoilTempArray){
+    this->SoilTempArray = _SoilTempArray;
+}
+void SoilTemperatureAuxiliary::setiSoilTempArray(std::vector<double> const &_iSoilTempArray){
+    this->iSoilTempArray = _iSoilTempArray;
+}
 void SoilTemperatureAuxiliary::setSnowIsolationIndex(double _SnowIsolationIndex) { this->SnowIsolationIndex = _SnowIsolationIndex; }
+void SoilTemperatureAuxiliary::setiSoilSurfaceTemperature(double _iSoilSurfaceTemperature) { this->iSoilSurfaceTemperature = _iSoilSurfaceTemperature; }
--- Models/Simplace_Soil_Temperature/src/cpp/Simplace_Soil_Temperature/SoilTemperatureAuxiliary.h
+++ Models/Simplace_Soil_Temperature/src/cpp/Simplace_Soil_Temperature/SoilTemperatureAuxiliary.h
@@ -8,11 +8,20 @@ namespace Simplace_Soil_Temperature {
 class SoilTemperatureAuxiliary
 {
     private:
+        std::vector<double> SoilTempArray ;
+        std::vector<double> iSoilTempArray ;
         double SnowIsolationIndex ;
+        double iSoilSurfaceTemperature ;
     public:
         SoilTemperatureAuxiliary();
+        std::vector<double> & getSoilTempArray();
+        void setSoilTempArray(const std::vector<double> &  _SoilTempArray);
+        std::vector<double> & getiSoilTempArray();
+        void setiSoilTempArray(const std::vector<double> &  _iSoilTempArray);
         double getSnowIsolationIndex();
         void setSnowIsolationIndex(double _SnowIsolationIndex);
+        double getiSoilSurfaceTemperature();
+        void setiSoilSurfaceTemperature(double _iSoilSurfaceTemperature);
 
 };
 }
